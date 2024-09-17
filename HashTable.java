0bb9d5d2bd352c32d774bf472030b965230f4687
Lasy <47871724+lramovha@users.noreply.github.com>
2024-09-17 22:57:28 +0200

    hashTable
    creating hashtables from scratch

--- HashTable.java
+++ HashTable.java
@@ -1,12 +1,40 @@
 public class HashTable {
     private static final int TABLE_SIZE = 10;
-    private String [] keys;
-    private int [] values;
+    private int [] table;
 
     public HashTable() {
-        keys = new String[TABLE_SIZE];
-        values = new int[TABLE_SIZE];
+        table = new int[TABLE_SIZE];
     }
     
-    
+    public int hash(String key) {
+        return key.hashCode() % TABLE_SIZE;
+    }
+
+    public void put(String key, int value) {
+        int index = hash(key);
+        table[index] = value;
+    }
+
+    public int get(String key) {
+        int index = hash(key);
+        return table[index];
+    }
+
+    public void printTable() {
+        for (int i = 0; i < TABLE_SIZE; i++) {
+            System.out.println(i + ": " + table[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        HashTable hashTable = new HashTable();
+        hashTable.put("John", 123456);
+        hashTable.put("Jane", 234567);
+        hashTable.put("Ramovha", 781568);
+        hashTable.put("Lasy", 345678);
+        hashTable.put("Mash", 365464);
+        
+        hashTable.printTable();
+    }
+
 }
